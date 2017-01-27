fbeda6cf5c8f148e45a8bc33e8c78f63ee5ee36f
LuckyHJH <luckyhjh@qq.com>
2017-01-27 17:46:11 +0800

    优化流程。另外官网于161221修改了页面，于是要对应修改才能继续抓取内容

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -37,16 +37,27 @@ class IndexController extends Controller {
 			throw new \Exception('网络未连接');
 		}
 		$html = $Html->find('html',0)->innertext;
-		preg_match('/g_img={.*?"(http.*?)".*?}/i', $html, $match);
+		//preg_match('/g_img={.*?"(http.*?)".*?}/i', $html, $match);//20160905
+		preg_match('/g_img={url.*?"(.*?)"/i', $html, $match);//20161221
 		$image = $match[1];
 		$title = $Html->find('#sh_cp',0)->getAttribute('title');
 		$title = substr($title, 0, strpos($title, ' ('));
+		if (empty($image) || empty($image)) {
+			throw new \Exception('无法分析数据');
+		}
 
 		//保存文件
 		$ext = substr($image, strrpos($image, '.'));
 		$file = $this->_imgFolder.$title.$ext;
 		if (!file_exists($file)) {
-			if (!file_put_contents($file, file_get_contents($image))) {
+			if (!strstr($path,'://')) {
+				$image = $url.$image;
+			}
+			$file_content = file_get_contents($image);
+			if (empty($file_content)) {
+				throw new \Exception('抓取图片失败');
+			}
+			if (!file_put_contents($file, $file_content)) {
 				throw new \Exception('无法写入文件');
 			}
 		}
